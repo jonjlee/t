b3821e4edf1fb91b566abe618a1bd4c056d29441
Jonathan Lee <jonjlee@gmail.com>
2016-04-29 13:12:23 -0500

    Add keymaster.js for keyboard shortcuts. Better extraction for preprocessing <script>s.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ function initUI() {
   // Initialize editors
   tinymce.init({
     selector: '.editor',
-    plugins: ['lists table contextmenu paste'],
+    plugins: ['lists table paste'],
     toolbar: false, //'bold italic | bullist numlist outdent indent | link image',
     menubar: false,
     statusbar: false,
+    setup: function(editor) {
+      editor.addShortcut('ctrl+13, meta+13', 'Process', submitNote);
+    }
   });
   
   // Initialize js console
@@ -31,13 +34,15 @@ function initUI() {
   
   // Initialize handlers
   $('#submit-note').click(submitNote);
+
+  // Keyboard shortcuts
+  key('ctrl+enter, ⌘+enter', submitNote);
 }
 
 function submitNote() {
   var text = tinymce.get('editor-in').getContent({format: 'text'}),
       html = tinymce.get('editor-in').getContent({format: 'html'}),
-      unescaped = _.unescape(html),
-      preprocessed = preprocess(text, unescaped),
+      preprocessed = preprocess(text, html),
       compiled = _.template(preprocessed, {
         variable: 'd'
       });
@@ -45,16 +50,44 @@ function submitNote() {
 }
 
 function preprocess(text, html, delimiters) {
-  delimiters = delimiters || ['<%%', '%>'];
-  var code = text.match(new RegExp(delimiters[0] + '((?:.|[\r\n])*?)' + delimiters[1]));
-  if (code) {
-    try {
-      window.eval(code[1]);
-    } catch (e) {
-      console.log(e);
-    }
-  } 
-  return html.replace(new RegExp(delimiters[0] + '.*?' + delimiters[1] + '\s*(:?<br />)?'), '');
+  delimiters = delimiters || ['<script>', '</script>'];
+  
+  var script = getScripts(text, delimiters),
+      noscript = removeScripts(html, delimiters),
+      unescaped = unescapeTemplateTags(noscript);
+
+  return unescaped;
+}
+
+function getScripts(text, delimiters) {
+  var newlines = /\r?\n/g,
+      matcher = new RegExp(delimiters[0] + '([\\s\\S]*?)' + delimiters[1], 'g'),
+      section = '',
+      script = '';
+
+  while (section = matcher.exec(text)) {
+    script += section[1] + '; ';
+  }
+
+  return _.template('<% ' + script + ' %>');
+}
+
+function removeScripts(html, delimiters) {
+  delimiters = _.map(delimiters, _.escape);
+  var matcher = new RegExp(delimiters[0] + '([\\s\\S]*?)' + delimiters[1] + '(?:\\s*<br\\s*/>)?', 'g');
+  return html.replace(matcher, '');
+}
+
+function unescapeTemplateTags(html) {
+  var tags = ['<%', '<%=', '<%-', '%>'],
+      escaped = _.map(tags, _.escape).join('|'),
+      matcher = new RegExp(escaped, 'g');
+  
+  html = html.replace(matcher, function(match, offset) {
+    return _.unescape(match);
+  });
+  
+  return html;
 }
 
 jQuery(document).ready(function($) {
